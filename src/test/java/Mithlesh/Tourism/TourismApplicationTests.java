9144d732ba8db9e4d038432fa9ffb03f45d20370
techiemithlesh <techie.mithlesh@gmail.com>
2024-07-29 21:11:40 +0530

    Project Initialised

---
+++ src/test/java/Mithlesh/Tourism/TourismApplicationTests.java
@@ -0,0 +1,13 @@
+package Mithlesh.Tourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
